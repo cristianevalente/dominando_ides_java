82a7b372f3fd21b495a7d95a43a3e563cbab5021
cristiane <cristianevalente@hotmail.com>
2022-07-16 08:17:16 -0300

    My first commit.

---
+++ src/br/com/dio/Primeiro_Programa.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+public class Primeiro_Programa {
+
+	public static void main(String[] args) {
+		int a=7;
+		int b=8;
+		// TODO Auto-generated method stub
+		System.out.println("Hello Titi. Saudades. " + (a+b));
+	}
+
+}
